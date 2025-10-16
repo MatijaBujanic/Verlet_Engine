36d10309add6a904eb5f009fe554c551bcdc14bf
MatijaBujanic <62101103+MatijaBujanic@users.noreply.github.com>
2025-10-16 19:35:08 +0200

    Intermitten commit
    Commiting files before finalization, adding CMAKE

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
-#include <iostream>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 
-int main() { return 0; }
+int32_t main(int32_t, char *[]) {
+
+  constexpr int32_t window_height = 1000;
+  constexpr int32_t window_width = 1000;
+  sf::ContextSettings settings;
+  settings.antiAliasingLevel = 1;
+
+  sf::VideoMode videoMode;
+  videoMode.size = {window_width, window_height};
+
+  sf::RenderWindow window(videoMode, "Verlet");
+  const uint32_t frame_rate = 60;
+}
--- src/solver.hpp
+++ src/solver.hpp
@@ -5,7 +5,86 @@
 #include <vector>
 
 struct VerletObject {
-  sf::Vector2f position_current;
-  sf::Vector2f postition_old;
+  sf::Vector2f position;
+  sf::Vector2f position_last;
   sf::Vector2f acceleration;
+  float radius = 10.f;
+  sf::Color color = sf::Color::White;
+
+  VerletObject() = default;
+  VerletObject(sf::Vector2f position_, float radius_)
+      : position{position_}, position_last{position_}, acceleration{0.0f, 0.0f},
+        radius{radius_} {}
+
+  void update(float dt) {
+    // compute how much we moved
+    const sf::Vector2f displacement = position - position_last;
+    // update position
+    position_last = position;
+    position = position + displacement + acceleration * (dt * dt);
+    // reset acceleration
+    acceleration = {};
+  }
+
+  void accelerate(sf::Vector2f a) { 
+    acceleration += a; 
+  }
+
+  void setVelocity(sf::Vector2f v, float dt) {
+    position_last = position - (v * dt);
+  }
+
+  void addVelocity(sf::Vector2f v, float dt) { 
+    position_last -= v * dt; 
+  }
+
+  [[nodiscard]]
+  sf::Vector2f getVelocity(float dt) const{
+    return (position-position_last) / dt;
+  }
+};
+
+
+
+
+class Solver
+{
+public:
+  Solver()=default;
+
+  VerletObject& addObject(sf::Vector2f position, float radius){
+    return m_objects.emplace_back(position,radius);
+  }
+
+private:
+  uint32_t m_sub_steps = 1;
+  sf::Vector2f m_gravity = {0.0f, 1000.0f};
+  sf::Vector2f m_constraint_center;
+  float m_constraint_radius = 100.0f;
+  std::vector<VerletObject> m_objects;
+  float m_time = 0.0f;
+  float m_frame_dt = 0.0f;
+
+  void applyGravity(){
+    for (auto& obj : m_objects){
+      obj.accelerate(m_gravity);
+    }
+  }
+  
+  void applyConstraint(){
+    for(auto& obj : m_objects){
+      const sf::Vector2f v = m_constraint_center - obj.position;
+      const float dist = std::sqrt(v.x*v.x+v.y*v.y);
+      if(dist > (m_constraint_radius - obj.radius)){
+        const sf::Vector2f n=v/dist;
+        obj.position = m_constraint_center - n *(m_constraint_radius - obj.radius);
+      }
+    }
+  }
+
+  void updateObjects(float dt){
+    for(auto& obj : m_objects){
+      obj.update(dt);
+    }
+  }
 };
